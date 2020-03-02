69a2cd694aecc561e9c2b7d0f54db67ab4e15851
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-03-02 09:19:02 +0100

    Add guix install file

---
+++ contrib/conductor.scm
@@ -0,0 +1,30 @@
+(define-module (conductor)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages check)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system python)
+  #:use-module (guix gexp)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26))
+
+(define %source-dir (dirname (dirname (current-filename))))
+
+(package
+  (name "conductor")
+  (version "0.1")
+  (source (local-file %source-dir #:recursive? #t))
+  (build-system python-build-system)
+  (propagated-inputs
+   `(("python-asyncssh" ,python-asyncssh)
+     ("python-yarl" ,python-yarl)
+     ("python-aionotify" ,python-aionotify)
+     ("python-pytest" ,python-pytest)))
+  (home-page "https://github.com/leibniz-psychology/conductor")
+  (synopsis #f)
+  (description #f)
+  (license #f))
+
