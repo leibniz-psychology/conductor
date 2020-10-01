leibniz-psychology/conductor

commit fcfac3ed566aed31f99e801ecd8872c42cad32e3

client: Remove null connect arguments
asyncssh changed behavior(?) and does not accept None as user any more.

conductor/client.py
@@ -101,7 +101,9 @@ class Client ():
 			return ExitCode.SOCKET_USED
 
 		writeJson (dict (state='connect', user=self.user, host=self.host, port=self.port))
-		async with asyncssh.connect (self.host, port=self.port, username=self.user) as conn:
+		# remove null arguments
+		connectArgs = dict (filter (lambda x: x[1], [('host', self.host), ('port', self.port), ('user', self.user)]))
+		async with asyncssh.connect (**connectArgs) as conn:
 			commandproc = await asyncio.create_subprocess_exec (self.command[0],
 					*self.command[1:], start_new_session=True,
 					stdout=subprocess.PIPE, stderr=subprocess.PIPE)

commit 32ad0b6a5e166a1354723c59edc93f72e9007533

client: Fix wrong connect argument name
Introduced by commit fcfac3ed566aed31f99e801ecd8872c42cad32e3

conductor/client.py
@@ -103,7 +103,7 @@ class Client ():
 
 		writeJson (dict (state='connect', user=self.user, host=self.host, port=self.port))
 		# remove null arguments
-		connectArgs = dict (filter (lambda x: x[1], [('host', self.host), ('port', self.port), ('user', self.user)]))
+		connectArgs = dict (filter (lambda x: x[1], [('host', self.host), ('port', self.port), ('username', self.user)]))
 		async with asyncssh.connect (**connectArgs) as conn:
 			commandproc = await asyncio.create_subprocess_exec (self.command[0],
 					*self.command[1:], start_new_session=True,